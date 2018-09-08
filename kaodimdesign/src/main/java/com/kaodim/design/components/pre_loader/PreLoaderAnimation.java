f0ec31fbc00923014d59aec4b75c670f27756e9a
Shasin <shasin89@gmail.com>
2018-09-08 19:50:36 +0800

    Modified preLoaderAnimation code to handle delays and minimum show time to avoid UI flickers

--- kaodimdesign/src/main/java/com/kaodim/design/components/pre_loader/PreLoaderAnimation.java
+++ kaodimdesign/src/main/java/com/kaodim/design/components/pre_loader/PreLoaderAnimation.java
@@ -7,29 +7,112 @@ import android.view.View;
 import com.kaodim.design.R;
 import com.shasin.notificationbanner.Banner;
 
+import static com.androidquery.util.AQUtility.postDelayed;
+
 public class PreLoaderAnimation {
 
-    private static PreLoaderAnimation instance = null;
+    private static final int MIN_SHOW_TIME = 500; // ms
+    private static final int MIN_DELAY = 500; // ms
 
-    private PreLoaderAnimation() {
-    }
+    static long mStartTime = -1;
 
-    public static PreLoaderAnimation getInstance() {
-        if (instance == null) {
-            instance = new PreLoaderAnimation();
+    static boolean mPostedHide = false;
+
+    static boolean mPostedShow = false;
+
+    static boolean  mDismissed = false;
+
+    static Banner banner;
+
+    private static final Runnable mDelayedHide = new Runnable() {
+
+        @Override
+        public void run() {
+            mPostedHide = false;
+            mStartTime = -1;
+            hide();
         }
-        return instance;
+    };
+
+    private static final Runnable mDelayedShow = new Runnable() {
+
+        @Override
+        public void run() {
+            mPostedShow = false;
+            if (!mDismissed) {
+                mStartTime = System.currentTimeMillis();
+            }
+        }
+    };
+
+
+    private static void show(View view, Activity activity) {
+        if (activity != null) {
+            banner = new Banner(view,activity);
+            banner.setAsDropDown(false);
+            banner.setFillScreen(true);
+            banner.setLayout(com.kaodim.design.R.layout.kdl_pre_loader);
+            banner.show();
+        }
+
     }
 
-    public static void showLoading(View view, Activity activity){
-
-        if(activity != null){
-            Banner.make(view,activity, R.layout.kdl_pre_loader,true);
-            Banner.getInstance().show();
+    private static void hide() {
+        if(banner!=null){
+            banner.dismissBanner();
         }
     }
 
-    public static void hideLoading(){
-        Banner.getInstance().dismissBanner();
+
+    /**
+     * Hide the progress view if it is visible. The progress view will not be
+     * hidden until it has been shown for at least a minimum show time. If the
+     * progress view was not yet visible, cancels showing the progress view.
+     */
+    public static synchronized void hideLoading() {
+        mDismissed = true;
+        mPostedShow = false;
+        long diff = System.currentTimeMillis() - mStartTime;
+        if (diff >= MIN_SHOW_TIME || mStartTime == -1) {
+            // The progress spinner has been shown long enough
+            // OR was not shown yet. If it wasn't shown yet,
+            // it will just never be shown.
+            if(banner!=null){
+                banner.dismissBanner();
+            }
+        } else {
+            // The progress spinner is shown, but not long enough,
+            // so put a delayed message in to hide it when its been
+            // shown long enough.
+            if (!mPostedHide) {
+                postDelayed(mDelayedHide, MIN_SHOW_TIME - diff);
+                mPostedHide = true;
+            }
+        }
+    }
+
+    /**
+     * Show the progress view after waiting for a minimum delay. If
+     * during that time, hide() is called, the view is never made visible.
+     */
+    public static synchronized void showLoading(final View view,final Activity activity) {
+        // Reset the start time.
+        mStartTime = -1;
+        mDismissed = false;
+        mPostedHide = false;
+        if (!mPostedShow) {
+            postDelayed(new Runnable() {
+
+                @Override
+                public void run() {
+                    mPostedShow = false;
+                    if (!mDismissed) {
+                        mStartTime = System.currentTimeMillis();
+                        show(view,activity);
+                    }
+                }
+            }, MIN_DELAY);
+            mPostedShow = true;
+        }
     }
 }
