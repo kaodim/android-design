65fbfa7c7abc9b1ea5a6e81505a08dd60ec3fd98
shasin89 <shasin89@gmail.com>
2019-07-05 17:35:43 +0800

    Added customizable calendar view component
    This library component provides customisable canelndar view. All alendar logics will be implemented in project app

---
+++ kaodimdesign/src/main/java/com/kaodim/design/components/calendarView/ui/MonthViewHolder.kt
@@ -0,0 +1,59 @@
+package com.kaodim.design.components.calendarView.ui
+
+import android.support.v7.widget.RecyclerView
+import android.view.View
+import android.view.ViewGroup
+import android.widget.LinearLayout
+import com.kaodim.design.components.calendarView.model.CalendarDay
+import com.kaodim.design.components.calendarView.model.CalendarMonth
+
+internal class MonthViewHolder constructor(
+    adapter: CalendarAdapter,
+    rootLayout: ViewGroup,
+    dayConfig: DayConfig,
+    private var monthHeaderBinder: MonthHeaderFooterBinder<ViewContainer>?,
+    private var monthFooterBinder: MonthHeaderFooterBinder<ViewContainer>?
+) : RecyclerView.ViewHolder(rootLayout) {
+
+    private val weekHolders = (1..6).map { WeekHolder(dayConfig) }
+
+    val headerView: View? = rootLayout.findViewById(adapter.headerViewId)
+    val footerView: View? = rootLayout.findViewById(adapter.footerViewId)
+    val bodyLayout: LinearLayout = rootLayout.findViewById(adapter.bodyViewId)
+
+    private var headerContainer: ViewContainer? = null
+    private var footerContainer: ViewContainer? = null
+
+    lateinit var month: CalendarMonth
+
+    init {
+        // Add week rows.
+        weekHolders.forEach {
+            bodyLayout.addView(it.inflateWeekView(bodyLayout))
+        }
+    }
+
+    fun bindMonth(month: CalendarMonth) {
+        this.month = month
+        headerView?.let { view ->
+            val headerContainer = headerContainer ?: monthHeaderBinder!!.create(view).also {
+                headerContainer = it
+            }
+            monthHeaderBinder?.bind(headerContainer, month)
+        }
+        footerView?.let { view ->
+            val footerContainer = footerContainer ?: monthFooterBinder!!.create(view).also {
+                footerContainer = it
+            }
+            monthFooterBinder?.bind(footerContainer, month)
+        }
+        weekHolders.forEachIndexed { index, week ->
+            week.bindWeekView(month.weekDays.getOrNull(index).orEmpty())
+        }
+    }
+
+    fun reloadDay(day: CalendarDay) {
+        weekHolders.map { it.dayHolders }.flatten().firstOrNull { it.day == day }?.reloadView()
+    }
+
+}
