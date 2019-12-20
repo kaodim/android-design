5aa688f43146be29cd495130afe4c2cde73d6602
Coyan Oh <cellius.yan@gmail.com>
2019-12-20 14:54:51 +0800

    Add icon in interactive panel

--- kaodimdesign/src/main/java/com/kaodim/design/components/InteractivePanel.java
+++ kaodimdesign/src/main/java/com/kaodim/design/components/InteractivePanel.java
@@ -2,10 +2,13 @@ package com.kaodim.design.components;
 
 import android.content.Context;
 import android.content.res.TypedArray;
+import android.graphics.drawable.Drawable;
+import android.support.v4.content.ContextCompat;
 import android.util.AttributeSet;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 
@@ -16,6 +19,7 @@ public class InteractivePanel extends RelativeLayout {
 
     RelativeLayout rlParent;
     TextView tvHint;
+    ImageView ivPanelImage;
 
     private String hint = "";
     private String borderType = "dotted";
@@ -53,21 +57,28 @@ public class InteractivePanel extends RelativeLayout {
         DateTimePickerDialog.DateTimePickerOptions options = new DateTimePickerDialog.DateTimePickerOptions();
         hint = typedArray.getString(R.styleable.InteractivePanel_panelHint);
         borderType = typedArray.getString(R.styleable.InteractivePanel_borderType);
-
+        int drawable = typedArray.getResourceId(R.styleable.InteractivePanel_src, 0);
 
         //Recycle the TypedArray (saves memory)
         typedArray.recycle();
 
         LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
         inflater.inflate(R.layout.kdl_element_interactive_panel_layout, this);
-        initComponents();
+        initComponents(context, drawable);
     }
 
-    private void initComponents() {
+    private void initComponents(Context context, int drawable) {
         rlParent = findViewById(R.id.rlParent);
         tvHint = findViewById(R.id.tvHint);
+        ivPanelImage = findViewById(R.id.ivPanelImage);
 
 //        rlParent.setBackgroundResource(R.drawable.kdl_rect_dotted);
+        if (drawable != 0) {
+            ivPanelImage.setVisibility(View.VISIBLE);
+            setIcon(ContextCompat.getDrawable(context, drawable));
+        } else {
+            ivPanelImage.setVisibility(View.GONE);
+        }
         setHint(hint);
         setBorderType(borderType);
         setEvents();
@@ -76,6 +87,10 @@ public class InteractivePanel extends RelativeLayout {
     public void setHint(String hint) {
         this.hint = hint;
         tvHint.setText(hint);
+    }
+
+    public void setIcon(Drawable drawable) {
+        ivPanelImage.setImageDrawable(drawable);
     }
 
     public void setBorderType(String borderType) {
